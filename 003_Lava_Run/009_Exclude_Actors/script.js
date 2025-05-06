a9b301013dffbbc74cc609ec7d0698bbbd0adaf3
kruckenberg <nkruckenberg@gmail.com>
2025-05-05 20:42:21 -0500

    lava-run: exclude actors

---
+++ 003_Lava_Run/009_Exclude_Actors/script.js
@@ -0,0 +1,57 @@
+const legend = {
+  ".": "empty",
+  "#": "wall",
+  "@": "player",
+  o: "coin",
+  "+": "lava",
+};
+
+const levelMap = `
+.##..............
+.#...........o.o.
+.#...............
+.#..........####.
+.#...@......#++#.
+.############++##
+`;
+
+const rows = levelMap
+  .trim()
+  .split("\n")
+  .map((row) => [...row]);
+
+const cols = rows[0].length;
+const size = 20;
+
+const actors = [];
+
+function createTd(className) {
+  const td = document.createElement("td");
+  td.className = className;
+  td.style.height = `${size}px`;
+
+  return td;
+}
+
+function createTr(cells) {
+  const tr = document.createElement("tr");
+
+  for (let cell of cells) {
+    let className = legend[cell];
+    tr.appendChild(createTd(className));
+  }
+
+  return tr;
+}
+
+function addRows(rows) {
+  const table = document.getElementById("background-table");
+  table.style.width = `${cols * size}px`;
+  const map = rows.map(createTr);
+
+  for (let row of map) {
+    table.appendChild(row);
+  }
+}
+
+addRows(rows);
